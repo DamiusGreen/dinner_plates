72dc160b5bff94836e5a35352977870b536870f7
damius <damius9@yahoo.com>
2019-10-13 22:37:56 -0400

    Updates

---
+++ precache-manifest.4e1eed2d241fc34ae431b6698029a36b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94c86f1d037f72dd4682b7fe20168fb",
+    "url": "/dinner_plates/index.html"
+  },
+  {
+    "revision": "72c2d9e9a876124957a5",
+    "url": "/dinner_plates/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/dinner_plates/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "72c2d9e9a876124957a5",
+    "url": "/dinner_plates/static/js/main.05341466.chunk.js"
+  },
+  {
+    "revision": "65dabbd363f15f604f0b",
+    "url": "/dinner_plates/static/js/runtime~main.25e1ad13.js"
+  }
+]);
